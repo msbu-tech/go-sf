046dae5a55a913fd4d53ca63cfc9236511026940
David Zhang <crispgm@gmail.com>
2016-09-19 19:35:01 +0800

    Add base commands

--- cmd/go-sf/main.go
+++ cmd/go-sf/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"flag"
-	
+
 	"github.com/msbu-tech/wundercafe/cmd/version"
 	"github.com/msbu-tech/go-sf/framework"
 )
@@ -11,9 +11,10 @@ import (
 var (
 	helpPtr     *bool = flag.Bool("help", false, "Help")
 	versionPtr  *bool = flag.Bool("version", false, "Version Info")
-	newPtr    *string = flag.String("new", "demo", "New App")
+	newPtr      *bool = flag.Bool("new", true, "New App")
+	namePtr   *string = flag.String("name", "demo", "App Name")
 	tplPtr    *string = flag.String("template", "service", "Template Path")
-	// outputPtr *string = flag.String("output", "./", "Output Path")
+	outputPtr *string = flag.String("output", "./", "Output Path")
 )
 
 func main() {
@@ -21,21 +22,30 @@ func main() {
 	flag.Usage = usage
 	flag.Parse()
 
+	if *helpPtr == true {
+		usage()
+
+		return
+	}
 	if *versionPtr == true {
 		fmt.Println("go-sf", version.Version)
 		fmt.Println("Copyright (c) MSBU-Tech, 2016")
-	} else if *helpPtr == true {
-		usage()
-	} else {
-		usage()
+
+		return
 	}
 
-	fmt.Print("new app: \n")
-	err := framework.NewApp(*newPtr, *tplPtr)
-	if err != nil {
-		fmt.Println(err)
+	if *newPtr == true {
+		fmt.Println("New App")
+		fmt.Println("\tApp Name: ", *namePtr)
+		fmt.Println("\tTemplate Path: ", *tplPtr)
+		fmt.Println("\tOutput Path: ", *outputPtr)
+
+		err := framework.NewApp(*namePtr, *tplPtr)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Success.")
 	}
-	fmt.Println("success!\n")
 }
 
 func usage() {
